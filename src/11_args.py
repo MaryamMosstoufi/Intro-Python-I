4dab18e74f7228bb6c6bf548828b1daee5299997
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-01 16:14:22 -0400

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(a, b):
+    return a+b
+
 
 print(f1(1, 2))
 
@@ -14,6 +17,14 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+
+def f2(*x):
+    sum = 0
+    for each in x:
+        sum = sum + each
+    return sum
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -22,7 +33,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +41,14 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+
+def f3(*x):
+    if len(x) == 1:
+        return x[0] + 1
+    else:
+        return x[0]+x[1]
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +63,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**data):
+    for key, value in data.items():
+        print("key: {}, value: {}".format(key, value))
+
 
 # Should print
 # key: a, value: 12
@@ -62,4 +85,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
